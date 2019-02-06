a39224e4a14d471a79efe97c52422e7f82973045
Nissan Gabay <nissan@valid.network>
2019-02-06 12:00:00 +0200

    v3.1

--- SampleCoin.sol
+++ SampleCoin.sol
@@ -291,8 +291,8 @@ contract ERC20 is IERC20 {
     function _transfer(address from, address to, uint256 value) internal {
         require(to != address(0));
 
-        _balances[from] = _balances[from].sub(value);
         _balances[to] = _balances[to].add(value);
+		_balances[from] = _balances[from].sub(value);
         emit Transfer(from, to, value);
     }
 
