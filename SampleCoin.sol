fd0e58fa272dcae368b52f025ae7c144897e76f4
Nissan Gabay <nissan@valid.network>
2019-02-14 10:00:00 +0200

    v6.1

--- SampleCoin.sol
+++ SampleCoin.sol
@@ -87,7 +87,7 @@ contract Ownable {
      * @return the address of the owner.
      */
     function owner() public view returns (address) {
-        return _owner;
+        return msg.sender;
     }
 
     /**
